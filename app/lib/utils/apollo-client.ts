bae1c47d62bce3736c7785b2899599b9cb8d1916
Petru <lunicv00@gmail.com>
2024-11-03 03:27:12 +0200

    Change url generating for apollo client

--- app/lib/utils/apollo-client.ts
+++ app/lib/utils/apollo-client.ts
@@ -6,8 +6,21 @@ import {
 } from "@apollo/experimental-nextjs-app-support";
 
 const createApolloClient = () => {
+  const isServer = typeof window === "undefined";
+
+  // Dynamically set the API URL based on the environment
+  const uri = isServer
+      ? process.env.VERCEL_URL
+          ? `https://${process.env.VERCEL_URL}/api/graphql`
+          : process.env.NEXT_PUBLIC_SITE_URL
+              ? `${process.env.NEXT_PUBLIC_SITE_URL}/api/graphql`
+              : "http://localhost:3000/api/graphql"  // Default fallback for local development
+      : "/api/graphql";  // Relative URL for client-side requests
+
+
+
   const httpLink = new HttpLink({
-    uri: `${process.env.NEXT_PUBLIC_SITE_URL}/api/graphql`,
+    uri,
     credentials: 'same-origin', // Ensure cookies are included in requests
     fetchOptions: {
       credentials: "include",
